f45bc7342cf2ec15792726e7f88a356ea9949037
VeronicaVassallo <vvassallo94@gmail.com>
2024-04-04 11:46:24 +0200

    second modify style css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,35 +10,42 @@ type Navprops = {
 
 export const Navbar = (props: Navprops) => {
 	const dataPlayers = useContext(PlayerContext);
-	const [current, setCurrent] = useState(dataPlayers.player);
+	const [current, setCurrent] = useState(0);
 
-	const changeCurrent = () => {
+	const changeCurrent = (): void => {
 		if (dataPlayers.player === 1) {
 			setCurrent(1);
-		} else {
+		} else if (dataPlayers.player === 2) {
 			setCurrent(2);
 		}
 	};
 
 	useEffect(() => {
 		changeCurrent();
-	}, [dataPlayers.player, current]);
+	}, [dataPlayers.player]);
 
 	return (
 		<div className="backgroundNav">
 			<h1>{props.title}</h1>
 			<div className="containerPlayerNav">
-				<div className={current === 1 ? "playerCurrent" : ""}>
-					<p>Giocatore 1</p>
-					<h2>X</h2>
-				</div>
+				{current === 1 ? (
+					<div className="playerCurrent">
+						<p>Giocatore 1</p>
+						<h2>X</h2>
+					</div>
+				) : (
+					""
+				)}
 
 				<ButtonRedresh />
-
-				<div className={current === 2 ? "playerCurrent" : ""}>
-					<p>Giocatore 2</p>
-					<h2>O</h2>
-				</div>
+				{current === 2 ? (
+					<div className="playerCurrent">
+						<p>Giocatore 2</p>
+						<h2>O</h2>
+					</div>
+				) : (
+					""
+				)}
 			</div>
 		</div>
 	);
